2bd5d908960a4d411bbd0975c09a59494bfc0b53
HariDharma10192 <haridharma173@gmail.com>
2023-10-20 18:58:06 +0800

    Model, Collection, & Controller

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('container')
+
+<h1>{{$nama}}</h1>
+<h3>{{$email}}</h3>
+<h3>{{$alamat}}</h3>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view('welcome', [
+        'test' => 'Hai Ini Test Dari Routes',
+        'title' => 'Welcome'
+    ]);
+});
+
+
+Route::get('/home', function () {
+    return view('home', [
+        'title' => 'Home'
+    ]);
+});
+
+Route::get('/blog', [PostController::class, 'index']);
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About',
+        'nama'  => 'Hari Dharma',
+        'email' => 'admin@gmail.com',
+        'alamat' => 'Jl. Raya Cimanggu No 123, Bandung',
+        'image'  => '../images/hari.jpg'
+    ]);
+});
+// halama single post
+Route::get('posts/{slug}', [PostController::class, 'show']);
