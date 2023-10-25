abb764506fce2e236b8b484a0017c955e4384c36
HariDharma10192 <haridharma173@gmail.com>
2023-10-26 07:20:43 +0800

    membuat PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,19 +7,21 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    // Metode index digunakan untuk menampilkan daftar postingan.
     public function index()
     {
         return view('posts', [
-            'title' => 'Blog',
-            'posts' => Post::all()
+            'title' => 'Blog', // Judul halaman, misalnya "Blog"
+            'posts' => Post::all() // Mengambil semua postingan dari model "Post" dan mengirimkannya ke tampilan.
         ]);
     }
 
+    // Metode show digunakan untuk menampilkan halaman tunggal untuk satu postingan.
     public function show(Post $post)
     {
         return view('post', [
-            "title" => "Single Post",
-            "post" => $post
+            "title" => "Single Post", // Judul halaman, misalnya "Single Post"
+            "post" => $post // Mengirim satu postingan (objek) yang telah diberikan sebagai parameter ke tampilan.
         ]);
     }
 }
