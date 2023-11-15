e2593f7c7ccc4372029f7bbfceb6d2f4a918b0eb
HariDharma10192 <haridharma173@gmail.com>
2023-11-15 11:33:35 +0800

    18. Create Post Form

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,26 @@
-<h1> Welcome Hari Dharma</h1>
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body pb-0 d-flex justify-content-between">
+                        <div>
+                            <h1 class="mb-1">Wellcome Back, {{ auth()->user()->name}}.</h1>
+                            {{-- <p>Total Earnings of the Month</p>
+                            <h3 class="m-0">$ 12,555</h3> --}}
+                            
+                        </div>
+                        
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
@@ -74,4 +74,13 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+Route::prefix('dashboard/posts')->middleware('auth')->group(function () {
+    Route::get('checkSlug/{slug}', [DashboardPostController::class, 'checkSlug'])->name('dashboard.posts.checkSlug');
+    Route::resource('/', DashboardPostController::class);
+});
+
+// Route::resource('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug']);
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
